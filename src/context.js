df7d4e486034c8e531354e6267551cd3481a8388
abhinavkr197 <abhinav.gupta@redgirraffe.com>
2019-12-17 15:32:45 +0530

    show detail product wise

--- src/context.js
+++ src/context.js
@@ -22,8 +22,16 @@ export default class ProductProvider extends Component {
             return {products: tempProducts}
         });
     };
-    handleDetail = () => {
-        console.log("hello from detail")
+    getItem = (id) => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
+    }
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {detailProduct:product}
+        })
+      
     };
     addToCart = (id) => {
         console.log(`hello from cart.id is ${id}` )
